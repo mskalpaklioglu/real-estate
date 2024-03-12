c1a2ce6401cb7de10f817916e8452087af3721e7
mskalpaklioglu <77413367+mskalpaklioglu@users.noreply.github.com>
2024-03-12 19:03:42 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import './App.css'
+import Companies from "./components/Companies/Companies";
+import Residencies from "./components/Residencies/Residencies";
+import Value from "./components/Value/Value";
+import Contact from "./components/Contact/Contact";
+import GetStarted from "./components/GetStarted/GetStarted";
+import Footer from "./components/Footer/Footer";
+
 function App() {
   return (
     <div className="App">
@@ -9,6 +16,12 @@ function App() {
         <Header/>
         <Hero />
       </div>
+      <Companies/>
+      <Residencies/>
+      <Value/>
+      <Contact/>
+      <GetStarted/>
+      <Footer/>
     </div>
   );
 }
